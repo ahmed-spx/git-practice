510fdeed19914f9d336bc098da74ee4e90830c88
main <ahmed.q2099@gmail.com>
2025-05-17 23:09:55 -0400

    bubble sort

--- practice.js
+++ practice.js
@@ -40,3 +40,19 @@ multiDimensionalArray.pop();
 // console.log(array);
 // array.unshift("I ");
 // console.log(array);
+
+function bubbleSort(array) {
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        var temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+}
+
+var newArray = [5, 3, 8, 1, 2];
+console.log(bubbleSort(newArray)); // sorts the array in ascending order
