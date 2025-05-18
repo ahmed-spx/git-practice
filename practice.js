f8beb6c7745f05d2c9e03d1e7e168936599940cf
main <ahmed.q2099@gmail.com>
2025-05-17 21:41:48 -0400

    Updated with variables

---
+++ practice.js
@@ -0,0 +1,6 @@
+var number = 5; // initialize the variable called 'number' with the value of 5
+var myName = "Kirin";
+let myAge = 20; // only exists where its initialized
+myAge = null;
+const pi = 3.14; // not able to be changed
+
