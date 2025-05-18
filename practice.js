30102c0675bd135b1efbad51286661b23e45ed08
main <ahmed.q2099@gmail.com>
2025-05-17 22:56:57 -0400

    functions and testing

--- practice.js
+++ practice.js
@@ -7,16 +7,36 @@ const pi = 3.14; // not able to be changed
 myAge = "I am ";
 
 myAge = myAge + number; // concatenation
-console.log(myAge); // prints 'I am 5'
+//console.log(myAge); // prints 'I am 5'
 
 number *= 0.3;
-console.log(number);
+//console.log(number);
 
-console.log("I love \"crazy\" girls"); // how to print quotations within a string
+//console.log("I love \"crazy\" girls"); // how to print quotations within a string
 
 var firstName = "Quandale Dingle Berryman the 3rd";
-console.log(firstName.length); // prints the length of the string
+//console.log(firstName.length); // prints the length of the string
 
 var thirdLetterInName = firstName[2];
-console.log(thirdLetterInName);
+//console.log(thirdLetterInName);
 
+function multiplication(a, b) {
+  return a * b;
+};
+//console.log(multiplication(number, 73)); // creates a function that multiples 73 by a variable
+
+var multiDimensionalArray = [ ["2", "5", "23"], ["67", "2", "75"], ["24", "13", "54"] ];
+//console.log(multiDimensionalArray[2][1]); // goes to the 3rd array, grabs 2nd element
+
+var list = ["alpha", "beta", "charlie", "delta"];
+list.push("echo"); // adds "echo" to the end of the array
+
+multiDimensionalArray.pop(); 
+//console.log(multiDimensionalArray); // prints the first array
+
+// var array = ["Mi ", "love ", "my ", "car"];
+// console.log(array);
+// array.shift();
+// console.log(array);
+// array.unshift("I ");
+// console.log(array);
