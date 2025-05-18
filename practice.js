ccb118a1a0fed79d6a0ed08ccd5c074e1c0f283e
main <ahmed.q2099@gmail.com>
2025-05-17 22:07:57 -0400

    String manipulation and logging

--- practice.js
+++ practice.js
@@ -4,3 +4,19 @@ let myAge = 20; // only exists where its initialized
 myAge = null;
 const pi = 3.14; // not able to be changed
 
+myAge = "I am ";
+
+myAge = myAge + number; // concatenation
+console.log(myAge); // prints 'I am 5'
+
+number *= 0.3;
+console.log(number);
+
+console.log("I love \"crazy\" girls"); // how to print quotations within a string
+
+var firstName = "Quandale Dingle Berryman the 3rd";
+console.log(firstName.length); // prints the length of the string
+
+var thirdLetterInName = firstName[2];
+console.log(thirdLetterInName);
+
